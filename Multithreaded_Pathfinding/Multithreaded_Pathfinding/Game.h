a8263eeca9c265b366233e2f1eed2013d4f04f23
Josh <c00224660@itcarlow.ie>
2021-05-12 17:31:42 +0100

    starter code fixed to run

--- Multithreaded_Pathfinding/Multithreaded_Pathfinding/Game.h
+++ Multithreaded_Pathfinding/Multithreaded_Pathfinding/Game.h
@@ -5,41 +5,7 @@
 // Extra ----------------------------
 #include <iostream>
 #include "SFML/Graphics.hpp"
-#include "SFML/Audio.hpp"
-#include "GUIGlobals.h"
-#include "MyVector3.h"
-#include "Xbox360Controller.h"
-#include <String>
-#include <ctime>
-#include "ResourceManager.h"
-#include "Player.h"
-#include "Enemy.h"
-#include <stdio.h>      /* printf, NULL */
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
 
-
-// Screens -------------------------
-#include "Licence.h"
-#include "Splash.h"
-#include "MainMenu.h"
-#include "Gameplay.h"
-#include "Options.h"
-#include "Rules.h"
-#include "SoundOptions.h"
-#include "Pause.h"
-#include "LevelEditor.h"
-#include "Winning.h"
-#include "GameOver.h"
-
-// GUI -----------------------------
-#include "CharacterInput.h"
-#include "CheckBox.h"
-#include "HorizontalSlider.h"
-#include "VerticalSlider.h"
-#include "Button.h"
-#include "RadioButton.h"
-#include "GUI.h"
 
 class Game
 {
@@ -47,18 +13,6 @@ public:
 	Game();
 	~Game();
 	void run();
-	// Intialized and commented in cpp -------------------------
-	static GameMode m_gameMode;
-	static GameMode m_previousMode;
-	static std::string m_currentSaveName;
-	static std::string m_currentLoadName;
-	static bool m_loading;
-	static bool m_saving;
-	static bool m_resetPlay;
-	static bool m_win;
-	static float m_screenChangeTimer;
-	static sf::View m_screenView;
-	static int MUSIC_VOLUME;
 
 protected:
 	void intialize();
@@ -66,39 +20,8 @@ protected:
 	void processInput();
 	void render();
 
-	// SFML parts ---------------------------
-	sf::Music m_mainMusic;
-	sf::RenderWindow m_renderWin;
-	sf::Text m_name;
-	sf::Font m_font;
-
-	/*Screens*/
-	Licence *m_licenceScreen;
-	Splash *m_splashScreen;
-	MainMenu *m_mainMenuScreen;
-	Gameplay *m_gameplayScreen;
-	Options *m_optionsScreen;
-	LevelEditor *m_levelEditor;
-	Rules *m_rulesScreen;
-	SoundOptions *m_soundOptionsScreen;
-	Winning* m_winningScreen;
-	Pause *m_pauseScreen;
-	GameOver* m_gameOverScreen;
-
-	GUI myGUI;
-	Widget * m_currentWidgetFocus{ nullptr };
-
-
 
 private:
-	MyVector3 m_mousePosition;
-	bool m_mouseClicked{ false };
-	bool m_mouseClickedRight{ false };
-	int m_scroll;
-	ResourceManager m_gameAssets;
-
-
-	Player m_player;
-	Enemy m_enemyOne;
+	sf::RenderWindow m_renderWin; // main SFML window
 };
 
--- Multithreaded_Pathfinding/Multithreaded_Pathfinding/Tile.cpp
+++ Multithreaded_Pathfinding/Multithreaded_Pathfinding/Tile.cpp
@@ -1,19 +1,7 @@
 #include "Tile.h"
 
-Tile::Tile(ResourceManager& t_gameAssets, std::string t_tag, int t_index, int t_direction) :
-	index{ t_index },
-	rotDirection{ t_direction }
+Tile::Tile()
 {
-	m_tileSprite.setTexture(t_gameAssets.m_gameTextures[t_tag]);
-	//int temp = rotDirection;
-	//m_tileSprite.setOrigin(32, 32);
-	//while (temp > 0)
-	//{
-	//	m_tileSprite.rotate(90);
-	//	temp--;
-	//}
-	//m_tileSprite.rotate(90);
-	//m_tileSprite.setOrigin(0, 0);
 }
 
 Tile::~Tile()
@@ -40,22 +28,3 @@ sf::Sprite* Tile::getSprite()
 	return &m_tileSprite;
 }
 
-int Tile::getIndex()
-{
-	return index;
-}
-
-void Tile::setLayer(Layer L)
-{
-	m_tileLayer = L;
-}
-
-void Tile::setRotation(int newRot)
-{
-	rotDirection = newRot;
-}
-
-Layer Tile::getLayer()
-{
-	return m_tileLayer;
-}
